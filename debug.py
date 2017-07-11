dfed86851dd9c8c888c6cd69687a50a13f2f1947
mbladel <marcobladel@gmail.com>
2017-07-11 10:11:07 +0200

    Unified commands

--- debug.py
+++ debug.py
@@ -7,3 +7,10 @@ class js_debugCommand(sublime_plugin.TextCommand):
 
   def write(self, arg):
     self.view.run_command("js_debug_write", { 'text': arg })
+
+class js_debug_writeCommand(sublime_plugin.TextCommand):
+  def run(self, edit, text):
+    settings = sublime.load_settings('JSDebug.sublime-settings')
+    command = settings.get('command')
+    symbol = settings.get('string_symbol')
+    self.view.insert(edit, self.view.sel()[0].begin(), 'console.' + command + '(' + symbol + text + symbol + ', ' + text + ');')
